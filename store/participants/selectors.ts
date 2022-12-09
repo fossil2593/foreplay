8ef48ac3fb6b6b83f6ea162618e9f3d02f6e80f0
Fossil 2593 <fossil2593@gmx.com>
2022-12-09 21:42:02 +0100

    simplify participants slice

--- store/participants/selectors.ts
+++ store/participants/selectors.ts
@@ -1,9 +1,3 @@
-import { createSelector } from "@reduxjs/toolkit";
 import { RootState } from "..";
 
-export const getParticipantsStore = (state: RootState) => state.participants;
-
-export const getParticipants = createSelector(
-  [getParticipantsStore],
-  (slice) => slice.participants
-);
+export const getParticipants = (state: RootState) => state.participants;
--- store/participants/slice.ts
+++ store/participants/slice.ts
@@ -1,35 +1,33 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { createParticipant } from "./create";
 
-export interface ParticipantsStore {
-  participants?: Participant[];
-}
+export type ParticipantsStore = Participant[];
 
 export interface Participant {
   key: string;
   name: string;
 }
 
-const initialState: ParticipantsStore = {};
+const initialState: ParticipantsStore = [];
 
 export const participantsSlice = createSlice({
   name: "participants",
   initialState,
   reducers: {
     setNumberOfParticipants: (state, action: PayloadAction<number>) => {
-      const participants = [];
+      const participants: Participant[] = [];
 
       for (let i = 0; i < action.payload; i++) {
         participants.push(createParticipant());
       }
 
-      state.participants = participants;
+      return participants;
     },
     updateParticipantName: (
       state,
       action: PayloadAction<{ key: string; name: string }>
     ) => {
-      state.participants = state.participants?.map((participant) => {
+      return state.map((participant) => {
         if (participant.key !== action.payload.key) {
           return participant;
         }
